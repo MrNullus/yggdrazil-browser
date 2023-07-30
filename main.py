7b2584e81da80db891f4da8496707bace4e5b953
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-07-30 01:48:19 -0300

    finish escope project

--- main.py
+++ main.py
@@ -4,21 +4,35 @@ from PyQt5.QtWidgets import *
 import sys
 from PyQt5.QtWebEngineWidgets import *
 
+
 # Main Class
 class MainWindow(QMainWindow):
+    def go_home(self):
+        self.browser.setUrl(QUrl('https://google.com'))
+
+
+    def go_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+
+    def update_url(self, url):
+        self.url_bar.setText(url.toString())
+
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        #--=== Initial Config
         #-- Instanciate web engine view
         self.browser = QWebEngineView()
         #-- Define default URL browser
         self.browser.setUrl(QUrl('https://google.com'))
-        #-- At initialize the windows stay in central of widget
+        #-- At initialize the windows stay in central widget
         self.setCentralWidget(self.browser)
-        #-- The window in show the aperence is maximized
-        self.showMaximized()
 
-        #-- Navbar
+
+        #--=== Navbar
         navbar = QToolBar()
         self.addToolBar(navbar)
 
@@ -27,10 +41,26 @@ class MainWindow(QMainWindow):
         btn_preview.triggered.connect(self.browser.back)
         navbar.addAction(btn_preview)
 
+        #-- Button Next
+        btn_next = QAction('->', self)
+        btn_next.triggered.connect(self.browser.forward)
+        navbar.addAction(btn_next)
+
         #-- Button Refresh
         btn_refresh = QAction('Recaregar', self)
         btn_refresh.triggered.connect(self.browser.reload)
         navbar.addAction(btn_refresh)
+
+        #-- Button Home
+        btn_home = QAction('Home', self)
+        btn_home.triggered.connect(self.go_home)
+        navbar.addAction(btn_home)
+
+        #-- Search Bar
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.go_url)
+        navbar.addWidget(self.url_bar)
+        self.browser.urlChanged.connect(self.update_url)
 
 
 # Create App
